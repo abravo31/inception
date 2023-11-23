855b8cc4dc0fd437368b7b6a7798cbf6fd14ca1a
Amanda Bravo <abysims31@gmail.com>
2023-11-23 11:02:44 -0500

    change some details in mariadb container and remove image names from the docker compose

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -8,11 +8,18 @@ while ! mysqladmin ping -uroot --silent; do
     sleep 1
 done
 
-# Configurer la base de données et l'utilisateur
-mysql -uroot -e "CREATE DATABASE wordpress_db;"
-mysql -uroot -e "CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'wordpress_password';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+# Vérifier si la base de données existe et la créer si ce n'est pas le cas
+if ! mysql -uroot -e "USE wordpress_db"; then
+    mysql -uroot -e "CREATE DATABASE wordpress_db;"
+fi
+
+# Vérifier si l'utilisateur existe et le créer si ce n'est pas le cas
+if ! mysql -uroot -e "SELECT 1 FROM mysql.user WHERE user = 'wordpress_user'"; then
+    mysql -uroot -e "CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'wordpress_password';"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';"
+    mysql -uroot -e "FLUSH PRIVILEGES;"
+fi
 
 # Arrêter MariaDB
 mysqladmin shutdown -uroot
+
